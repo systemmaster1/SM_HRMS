0d9a63c5401bb3bd9b4203e040b64e302bc558a1
systemmaster1 <connect@systemmaster.in>
2026-08-11 17:24:48 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/downloads/:path*",
+        headers: [
+          { key: "Cache-Control", value: "no-store, no-cache, must-revalidate, max-age=0" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+        ],
+      },
+    ];
+  },
 };
 export default nextConfig;
