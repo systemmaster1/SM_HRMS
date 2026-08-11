5dc55b1b97c5af2fee547cb48e2cd83e149b93ca
systemmaster1 <connect@systemmaster.in>
2026-08-11 16:22:52 +0530

    Delete src/app/download/android/route.ts

--- src/app/download/android/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.redirect(
-    "https://github.com/systemmaster/sm-hrms/releases/latest/download/SM-HRMS.apk",
-    { status: 302 }
-  );
-}
