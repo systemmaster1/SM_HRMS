d065a5275dc7916200c67b9ce0326cdfc6ae582a
systemmaster1 <connect@systemmaster.in>
2026-09-25 02:51:09 +0530

    Bump Android release to 1.8.1 after duty notification fix

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "in.systemmaster.hrms"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.8.0"
+        versionCode = 10
+        versionName = "1.8.1"
         buildConfigField("String", "WEB_APP_URL", "\"https://hrms.systemmaster.in\"")
     }
 
